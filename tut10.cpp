9c2da145071a7d99bdd32cc85fc1028263e3a0f0
Faria003 <fs4400493@gmail.com>
2024-06-01 22:13:13 +0600

    For, While and do-while loops

---
+++ tut10.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /* Loops in C++
+    There are 3 types of loops in C++
+    1. For loop
+    2. While loop
+    3. Do While loop*/
+
+    // For loop in C++
+
+    // for ( int i = 1 ; i <=100 ; i++)
+    // {
+    //     cout<<i<<endl;
+    // }
+
+    // While loop in C++
+    // int i = 1;
+    // while (i <= 100)
+    // {
+    //     cout << i << endl;
+    //     i++;
+    // }
+
+    // Do While loop in C++
+    // int i = 1;
+    // do 
+    // {
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+    // while(i<=100);
+
+
+
+    // Quiz in the tutorial " Multiplication Table"
+
+    
+
+    // int n ;
+    // cout<<" Enter the number :"<<endl;
+    // cin>>n;
+    // cout << " Multication tables of " << n << ":"<<endl;
+
+    //using For loop
+    // for( int i = 1 ; i<=10 ; i++)
+    // {
+    //     cout << i << " x " << n << " = " << i * n << endl;
+    // }
+
+    // using While loop
+    // int i = 1;
+    // while(i<=10)
+    // {
+    //     cout << i << " x " << n << " = " <<i*n<<endl;
+    //     i++;
+    // }
+
+    //using Do While loop
+    // int i = 1;
+    // do 
+    // {
+    //   cout << i << " x " << n << " = " <<i*n<<endl;
+    //     i++;  
+    // }
+    // while (i<=10);
+
+    
+
+
+    return 0;
+}

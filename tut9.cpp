9222fcdf1e218b65f04e816af254e125f6e2da84
Faria003 <fs4400493@gmail.com>
2024-06-01 22:11:06 +0600

    Control Structures, If Else and Switch-Case Statement

---
+++ tut9.cpp
@@ -0,0 +1,38 @@
+ Control Structures, If Else and Switch-Case Statement
+
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int age ;
+    cout<<" Enter your age :"<<endl;
+    cin>>age;
+    // // Selection control structure: If else-if else ladder
+    // if (age<18)
+    // {
+    //     cout<<" You can't come to the party!"<<endl;
+    // }
+    // else if (age==18)
+    // {
+    //     cout<<" You are a kid and you will get a kid pass to the party"<<endl;
+    // }
+    // else 
+    // {
+    //    cout<<" You can come to the party"<<endl;
+    // }
+
+    //Seclection control structure : Switch Case statements
+    // switch (age)
+    // {
+    // case 18:
+
+    //   cout<<"you are adult"<<endl;
+    //     break;
+    
+    // default:
+    //   cout<<"No special cases"<<endl;
+    //     break;
+    // }
+    return 0;
+}

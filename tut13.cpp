889b44ff57bdc04a67bd0477e7dbe074451ef6fc
Faria003 <fs4400493@gmail.com>
2024-06-01 22:22:27 +0600

    Arrays & Pointers Arithmetic

---
+++ tut13.cpp
@@ -0,0 +1,61 @@
+// Arrays & Pointers Arithmetic
+
+#include<iostream>
+using namespace std;
+
+int main () {
+    int marks [4] = {34 , 55 , 67 , 99};
+    // int mathmarks[4];
+    // mathmarks[0]=78;
+    // mathmarks[1]=86;
+    // mathmarks[2]=70;
+    // mathmarks[3]=93;
+
+    // cout<<"These are the marks of math :"<<endl;
+
+    // cout<<mathmarks[0]<<endl;
+    // cout<<mathmarks[1]<<endl;
+    // cout<<mathmarks[2]<<endl;
+    // cout<<mathmarks[3]<<endl;
+    // cout<<endl;
+    // cout<<"These are the marks :"<<endl;
+    // cout<<marks[0]<<endl;
+    // cout<<marks[1]<<endl;
+    // //  You can change the value of an array
+    // marks[2]=45;
+    // cout<<marks[2]<<endl;
+    // cout<<marks[3]<<endl;
+
+    // for ( int i = 0 ; i<4 ; i++)
+    // {
+    //     cout<<"the value of  marks "<< i <<" is "<<marks[i]<<endl;
+    // }
+    
+    //Quick quiz solve
+    // int i = 0 ;
+    // while (i<4)
+    // {
+    //   cout<<" The value of mark " << i <<" is "<<marks[i]<<endl;
+    //   i++;
+    // }
+
+    // int i = 0;
+    // do
+    // {
+    //   cout<<" The value of mark " << i <<" is "<<marks[i]<<endl;
+    //   i++;
+    // }
+    // while(i<4);
+
+    // Pointers and Arrays
+    // int* p = marks;
+    // cout<<*(p++)<<endl;
+    // cout<<*p<<endl;
+    // cout<<*(++p)<<endl;
+
+    // cout<<" The value of marks[0] is "<<*p<<endl;
+    // cout<<" The value of marks[1] is "<<*(p+1)<<endl;
+    // cout<<" The value of marks[2] is "<<*(p+2)<<endl;
+    // cout<<" The value of marks[3] is "<<*(p+3)<<endl;
+    return 0;
+}
